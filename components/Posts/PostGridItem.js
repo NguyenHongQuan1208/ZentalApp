d58637d3f58f5619cf3aaeb6e8b202f6ca452ef8
NguyenHongQuan1208 <nquan1208@gmail.com>
2025-02-14 16:57:38 +0700

    add private icon

--- components/Posts/PostGridItem.js
+++ components/Posts/PostGridItem.js
@@ -1,13 +1,14 @@
 import { useNavigation } from "@react-navigation/native";
-import { Pressable, Dimensions, StyleSheet } from "react-native";
+import { Pressable, Dimensions, StyleSheet, View } from "react-native";
 import { TASK_SECTIONS } from "../../data/dummy-data";
 import { GlobalColors } from "../../constants/GlobalColors";
 import { formatDistanceToNowStrict, parseISO } from "date-fns";
 import PostImage from "./PostImage";
+import { Ionicons } from "@expo/vector-icons"; // Import Ionicons from Expo
 
-// Lấy chiều rộng màn hình
+// Get screen width
 const screenWidth = Dimensions.get("window").width;
-const itemWidth = screenWidth / 3; // Mỗi item chiếm 1/3 chiều rộng màn hình
+const itemWidth = screenWidth / 3; // Each item occupies 1/3 of the screen width
 
 function PostGridItem({ item, currentUserId }) {
   const navigation = useNavigation();
@@ -15,6 +16,7 @@ function PostGridItem({ item, currentUserId }) {
   const userId = item?.uid || "";
   const imageUri = item?.imageUri || "";
   const sectionId = item?.sectionId || "";
+  const publicStatus = item?.publicStatus;
 
   const sectionColor =
     TASK_SECTIONS.find((section) => section.id === sectionId)?.color ||
@@ -40,7 +42,18 @@ function PostGridItem({ item, currentUserId }) {
   return (
     <Pressable onPress={navigateHandler} style={styles.container}>
       {typeof imageUri === "string" && imageUri.trim() !== "" && (
-        <PostImage imageUri={imageUri} style={styles.image} />
+        <View style={styles.imageContainer}>
+          <PostImage imageUri={imageUri} style={styles.image} />
+          {publicStatus === 0 && (
+            <View style={styles.iconBackground}>
+              <Ionicons
+                name="lock-closed"
+                size={20}
+                color={GlobalColors.pureWhite}
+              />
+            </View>
+          )}
+        </View>
       )}
     </Pressable>
   );
@@ -51,10 +64,23 @@ const styles = StyleSheet.create({
     width: itemWidth,
     aspectRatio: 1,
   },
+  imageContainer: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+  },
   image: {
     width: "100%",
     height: "100%",
     borderRadius: 0,
+  },
+  iconBackground: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    borderRadius: "50%",
+    padding: 5,
   },
 });
 
